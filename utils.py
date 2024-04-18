0ac1d01b5ce0850ef2ff27213b9a280f1ef46f7d
ChinmayK0607 <114411195+ChinmayK0607@users.noreply.github.com>
2024-04-18 23:21:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from string import ascii_letters
 import random
-
+from queue import Queue
 
 def generate_room_code(length: int, existing_codes: list[str]) -> str:
     while True:
@@ -9,3 +9,15 @@ def generate_room_code(length: int, existing_codes: list[str]) -> str:
 
         if code not in existing_codes:
             return code
+
+def find_room(room_code:str, room_queue: dict) -> int:
+    for id in range(len(room_queue)):
+        if str(room_queue[id]) == room_code:
+            return id
+
+def init_room_queues(ROOMS: list, room_queue: dict):
+    for room in ROOMS:
+        room_name = Queue()
+        room_queue[room] = room_name
+
+
